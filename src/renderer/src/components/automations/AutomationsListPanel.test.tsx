0279fe3813774d2d4cbd46976fcb90bcbd752310
Jinjing <6427696+AmethystLiang@users.noreply.github.com>
2026-08-26 13:38:39 -0700

    Remove unused canCreateAutomation prop from test

--- src/renderer/src/components/automations/AutomationsListPanel.test.tsx
+++ src/renderer/src/components/automations/AutomationsListPanel.test.tsx
@@ -99,7 +99,6 @@ function renderPanel(
           requestExternalAction={() => undefined}
           openEditExternalDialog={() => undefined}
           openCreateDialog={() => undefined}
-          canCreateAutomation
           onOpenDetail={() => undefined}
           onRefresh={() => undefined}
           isRefreshing={false}
