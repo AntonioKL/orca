9a15f5090e610b4567ed3ca7d268751b9b3d5c53
Jinwoo Hong <73622457+Jinwoo-H@users.noreply.github.com>
2026-08-27 23:27:39 -0700

    fix(browser-preview): enforce canonical runtime grants (#16975)
    (cherry picked from commit fc8c9811037c40cb137611d9a27aeb41d9d58ca5)

--- src/main/runtime/orca-runtime.ts
+++ src/main/runtime/orca-runtime.ts
@@ -10774,6 +10774,8 @@ export class OrcaRuntimeService {
   }
   readFileExplorerPreview: RuntimeFileCommands['readFileExplorerPreview'] =
     this.fileCommands.readFileExplorerPreview.bind(this.fileCommands)
+  readDocPreviewFile: RuntimeFileCommands['readDocPreviewFile'] =
+    this.fileCommands.readDocPreviewFile.bind(this.fileCommands)
   readFileExplorerChunk: RuntimeFileCommands['readFileExplorerChunk'] =
     this.fileCommands.readFileExplorerChunk.bind(this.fileCommands)
   writeFileExplorerFile: RuntimeFileCommands['writeFileExplorerFile'] =
